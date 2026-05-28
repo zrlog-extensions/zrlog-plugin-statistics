4954594d514f5794c708f171120cc684d5ef9e01
xiaochun <90xchun@gmail.com>
2026-05-28 13:51:47 +0800

    add

--- src/main/java/com/zrlog/plugin/statistics/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/statistics/GraalvmAgentApplication.java
@@ -11,17 +11,14 @@ import com.zrlog.plugin.statistics.model.StatisticsLogStore;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class GraalvmAgentApplication {
 
 
     public static void main(String[] args) throws IOException {
-        new Gson().toJson(new HttpRequestInfo());
-        new Gson().toJson(new Plugin());
-        new Gson().toJson(new StatisticsConfig());
-        new Gson().toJson(new StatisticsLogEntry());
-        new Gson().toJson(new StatisticsLogStore());
+        PluginNativeImageUtils.gsonNativeAgentByClazz(Arrays.asList(StatisticsConfig.class, StatisticsLogEntry.class, StatisticsLogStore.class));
         String basePath = System.getProperty("user.dir").replace("\\target","").replace("/target", "");
         File file = new File(basePath + "/src/main/resources");
         PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath()  + "/", "/");
